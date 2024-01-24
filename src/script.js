8169cafa2575d5c00536ae5e4506c3816a3b3f92
Daiene <daiene.montuani@hotmail.com>
2024-01-23 23:16:29 -0300

    Ascending and descending ordering for the number of visits

--- src/script.js
+++ src/script.js
@@ -90,17 +90,40 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-    //Ordenar linhas
-    // Selecionar todas as células da coluna "Visits"
-    var visitsCells = document.querySelectorAll(".visits");
+    // Adiciona evento de clique ao ícone de classificação
+    const sortIcon = document.querySelectorAll(".fa-sort");
+    let sortDirection = 'asc'; // Estado inicial: ordenação crescente
 
-    // Extrair os valores e armazená-los em um array
-    var visitsValues = Array.from(visitsCells).map(function (cell) {
-        return cell.textContent;
+    sortIcon.forEach(icon => {
+        icon.addEventListener("click", function () {
+            const columnIndex = Array.from(this.parentNode.parentNode.children).indexOf(this.parentNode);
+
+            // Obtém os valores de visitas da coluna clicada
+            const visitsValues = Array.from(document.querySelectorAll(`.visits`)).map(cell => parseInt(cell.textContent));
+
+            // Ordena as linhas com base nos valores de visitas e na direção da ordenação
+            const sortedRows = Array.from(filteredRows)
+                .sort((a, b) => {
+                    const aValue = parseInt(a.children[columnIndex].textContent);
+                    const bValue = parseInt(b.children[columnIndex].textContent);
+
+                    if (sortDirection === 'asc') {
+                        return aValue - bValue;
+                    } else {
+                        return bValue - aValue;
+                    }
+                });
+
+            // Atualiza as linhas na tabela com as linhas ordenadas
+            table.querySelector("tbody").innerHTML = "";
+            sortedRows.forEach(row => {
+                table.querySelector("tbody").appendChild(row);
+            });
+
+            // Alterna a direção da ordenação para o próximo clique
+            sortDirection = (sortDirection === 'asc') ? 'desc' : 'asc';
+        });
     });
-
-    // Imprimir os valores
-    console.log(visitsValues);
 
 
     function updatePagination() {
